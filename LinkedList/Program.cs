ff9f87c98bd5c18106d0796efd460bc906e2d2d5
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-25 16:39:18 +0530

    UC1

---
+++ LinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
